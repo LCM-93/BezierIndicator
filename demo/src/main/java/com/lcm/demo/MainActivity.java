5e77e61052d789a115ccda2bec1d14cd49aa2856
李晨鸣 <lichenming@link-pay.cn>
2017-08-14 18:09:12 +0800

    加入CardViewPager

--- demo/src/main/java/com/lcm/demo/MainActivity.java
+++ demo/src/main/java/com/lcm/demo/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.lcm.bezierbottomIndicator.BezierBottomIndicator;
+import com.lcm.bezierbottomIndicator.viewpager.CardAdapter;
+import com.lcm.bezierbottomIndicator.viewpager.ShadowTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,36 +22,48 @@ public class MainActivity extends AppCompatActivity {
     private BezierBottomIndicator bezierBottomIndicator;
 
     private ViewPager viewPager;
-    private List<View> viewList;
+//    private List<View> viewList;
+    private List<Integer> images;
 
     private MyAdapter myAdapter;
+    private ShadowTransformer mCardShadowTransformer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewList = new ArrayList<>();
+//        viewList = new ArrayList<>();
+        images = new ArrayList<>();
 
         bezierBottomIndicator = (BezierBottomIndicator) findViewById(R.id.bezierBottomIndicator);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
 
 
-        ImageView imageView1 = new ImageView(getApplicationContext());
-        imageView1.setImageResource(R.mipmap.pos0);
-        viewList.add(imageView1);
-        ImageView imageView2 = new ImageView(getApplicationContext());
-        imageView2.setImageResource(R.mipmap.pos1);
-        viewList.add(imageView2);
-        ImageView imageView3 = new ImageView(getApplicationContext());
-        imageView3.setImageResource(R.mipmap.pos2);
-        viewList.add(imageView3);
-        ImageView imageView4 = new ImageView(getApplicationContext());
-        imageView4.setImageResource(R.mipmap.pos3);
-        viewList.add(imageView4);
+        images.add(R.mipmap.pos0);
+        images.add(R.mipmap.pos1);
+        images.add(R.mipmap.pos2);
+        images.add(R.mipmap.pos3);
 
-        myAdapter = new MyAdapter();
+//        ImageView imageView1 = new ImageView(getApplicationContext());
+//        imageView1.setImageResource(R.mipmap.pos0);
+//        viewList.add(imageView1);
+//        ImageView imageView2 = new ImageView(getApplicationContext());
+//        imageView2.setImageResource(R.mipmap.pos1);
+//        viewList.add(imageView2);
+//        ImageView imageView3 = new ImageView(getApplicationContext());
+//        imageView3.setImageResource(R.mipmap.pos2);
+//        viewList.add(imageView3);
+//        ImageView imageView4 = new ImageView(getApplicationContext());
+//        imageView4.setImageResource(R.mipmap.pos3);
+//        viewList.add(imageView4);
+
+        myAdapter = new MyAdapter(images);
+
+        mCardShadowTransformer = new ShadowTransformer(viewPager, myAdapter);
         viewPager.setAdapter(myAdapter);
+        viewPager.setPageTransformer(false,mCardShadowTransformer);
+        viewPager.setOffscreenPageLimit(3);
 
         bezierBottomIndicator.setCircularColors(Color.parseColor("#FCC04D"), Color.parseColor("#00C3E2"), Color.parseColor("#FE626D"), Color.parseColor("#966ACF"));
 
@@ -57,32 +72,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    class MyAdapter extends PagerAdapter {
-
-        @Override
-        public int getCount() {
-            return 4;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(viewList.get(position));
-            return viewList.get(position);
-        }
-
-        @Override
-        public int getItemPosition(Object object) {
-            return POSITION_NONE;
-        }
-    }
 }
---
+++ demo/src/main/java/com/lcm/demo/MyAdapter.java
@@ -0,0 +1,93 @@
+package com.lcm.demo;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.lcm.bezierbottomIndicator.viewpager.CardAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ****************************************************************
+ * Author: LCM
+ * Date: 2017/8/14 下午5:22
+ * Desc:
+ * *****************************************************************
+ */
+
+public class MyAdapter extends PagerAdapter implements CardAdapter {
+
+    private float mBaseElevation;
+    private List<Integer> images;
+    private List<CardView> cardViewList;
+
+    public MyAdapter(List<Integer> images) {
+        this.images = images;
+        cardViewList = new ArrayList<>();
+        for (int i = 0; i < images.size(); i++) {
+            cardViewList.add(null);
+        }
+    }
+
+    @Override
+    public float getBaseElevation() {
+        return mBaseElevation;
+    }
+
+    @Override
+    public CardView getCardViewAt(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+        cardViewList.set(position, null);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext())
+                .inflate(R.layout.item_viewpager, container, false);
+        container.addView(view);
+
+        bind(images.get(position), view);
+
+        CardView cardView = (CardView) view.findViewById(R.id.cardView);
+
+        if (mBaseElevation == 0) {
+            mBaseElevation = cardView.getCardElevation();
+        }
+
+        cardView.setMaxCardElevation(mBaseElevation * MAX_ELEVATION_FACTOR);
+        cardViewList.set(position, cardView);
+        return view;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+
+        return POSITION_NONE;
+    }
+
+
+    private void bind(int img, View view) {
+        ImageView imageView = (ImageView) view.findViewById(R.id.iv_image);
+        imageView.setImageResource(img);
+    }
+}
